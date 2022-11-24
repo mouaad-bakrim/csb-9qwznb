c45ff36d555d03a89a442fcab58ac28aa973b56c
Code Sandbox <hello@codesandbox.io>
2022-11-24 17:34:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.5e3bd701bf9c1a8536b83c816e7021c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c3639b8cf8db03287f2cfde2a013fb",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/index.html"
+  },
+  {
+    "revision": "3b938de4ad242d4166b0",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/static/css/main.32472ef0.chunk.css"
+  },
+  {
+    "revision": "ae6b2959392cdd6973ad",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/static/js/2.d2b5e9df.chunk.js"
+  },
+  {
+    "revision": "a62cce0860e48996e8ed9c85e1621a57",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/static/js/2.d2b5e9df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b938de4ad242d4166b0",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/static/js/main.157af194.chunk.js"
+  },
+  {
+    "revision": "94b4b2e3c355b8de010b",
+    "url": "https://mouaad-bakrim.github.io/csb-9qwznb/static/js/runtime-main.b8b03638.js"
+  }
+]);
